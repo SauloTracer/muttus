1baca71c6f7fb4901bcbedf1d8426d95920a4ad6
Saulo Machado <saulo.tracer+git@gmail.com>
2016-08-28 22:26:16 -0300

    Alterando path no arquivo de configuração

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/honeycomb/';
+$config['base_url'] = 'http://localhost/muttus/';
 
 /*
 |--------------------------------------------------------------------------
